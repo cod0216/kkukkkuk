6ac479cfbd98d938f73d71b47b6ec74ab4896528
hyeonjaez <jhjh0022@naver.com>
2025-03-10 08:50:37 +0900

    feat: backend project

---
+++ backend/KKUKKKUK/src/main/java/com/KKUKKKUK/KkukkkukApplication.java
@@ -0,0 +1,13 @@
+package com.KKUKKKUK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkukkkukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkukkkukApplication.class, args);
+	}
+
+}
---
+++ backend/KKUKKKUK/src/test/java/com/KKUKKKUK/KkukkkukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KKUKKKUK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkukkkukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
