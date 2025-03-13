8fc1eceac9b7f48d3ad7300caf3422a920db3e11
hyeonjaez <jhjh0022@naver.com>
2025-03-13 11:45:25 +0900

    feat: wallet entity mapping

---
+++ backend/KKUKKKUK/src/main/java/com/be/KKUKKKUK/domain/wallet/entity/Wallet.java
@@ -0,0 +1,55 @@
+package com.be.KKUKKKUK.domain.wallet.entity;
+
+import com.be.KKUKKKUK.domain.owner.entity.Owner;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+/**
+ * packageName    : com.be.KKUKKKUK.domain.wallet.entity<br>
+ * fileName       : Wallet.java<br>
+ * author         : Fiat_lux<br>
+ * date           : 2025-03-13<br>
+ * description    :  wallet entity class 입니다.<br>
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 25.03.13          Fiat_lux           최초생성<br>
+ */
+@Entity
+@Table(name = "wallet")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Wallet {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "private_key", nullable = false)
+    private String privateKey;
+
+    @Column(name = "public_key", nullable = false)
+    private String publicKey;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String did;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id", nullable = false)
+    private Owner owner;
+}
