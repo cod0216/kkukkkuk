5a66fead7a14d971d4d9a590aa0f444503b3dcda
송명석 <sms4621875@naver.com>
2025-03-14 16:03:27 +0900

    chore: ndk version 설정
    Flutter Secure Storage 사용을 위한 최소버전(27.0.12077973) 설정

--- clientend/android/app/build.gradle.kts
+++ clientend/android/app/build.gradle.kts
@@ -8,7 +8,8 @@ plugins {
 android {
     namespace = "com.ssafy.kkuk_kkuk"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    //ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
