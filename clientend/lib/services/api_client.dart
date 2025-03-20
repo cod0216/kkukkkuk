11eda5054e304ccd21224722309dc61e968f17fa
송명석 <sms4621875@naver.com>
2025-03-20 15:58:56 +0900

    feat: token interceptor
    dio client jwt token 관리 기능 구현

--- clientend/lib/services/api_client.dart
+++ clientend/lib/services/api_client.dart
@@ -1,12 +1,22 @@
+import 'dart:async';
+
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kkuk_kkuk/services/token_manager.dart';
 
 /// API 통신을 담당하는 클라이언트
 class ApiClient {
   final Dio _dio;
+  final TokenManager _tokenManager;
   final String baseUrl = 'https://kukkkukk.duckdns.org';
 
-  ApiClient() : _dio = Dio() {
+  // 토큰 재발급 중인지 여부
+  bool _isRefreshing = false;
+
+  // 토큰 재발급 중 대기 중인 요청들
+  final List<_RequestRetry> _pendingRequests = [];
+
+  ApiClient(this._tokenManager) : _dio = Dio() {
     _dio.options.baseUrl = baseUrl;
     _dio.options.connectTimeout = const Duration(seconds: 10);
     _dio.options.receiveTimeout = const Duration(seconds: 10);
@@ -15,9 +25,64 @@ class ApiClient {
       'Accept': 'application/json',
     };
 
-    // TODO: 토큰 인터셉터 구현
-    // TODO: 토큰 갱신 로직 구현
-    // TODO: 요청 재시도 로직 구현
+    // 토큰 인증 인터셉터 추가
+    _dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) async {
+          final token = await _tokenManager.getAccessToken();
+          if (token != null) {
+            options.headers['Authorization'] = 'Bearer $token';
+          }
+
+          return handler.next(options);
+        },
+        onError: (DioException error, handler) async {
+          // 401 에러 처리 (토큰 만료)
+          if (error.response?.statusCode == 401) {
+            try {
+              final requestOptions = error.requestOptions;
+
+              // 토큰 재발급 요청 중이면 대기열에 추가
+              if (_isRefreshing) {
+                final completer = _addToPendingRequests(requestOptions);
+                return handler.resolve(await completer.future);
+              }
+
+              _isRefreshing = true;
+
+              // 토큰 재발급 시도
+              final isRefreshed = await _refreshToken();
+
+              if (isRefreshed) {
+                // 토큰 재발급 성공, 원래 요청 재시도
+                final token = await _tokenManager.getAccessToken();
+                requestOptions.headers['Authorization'] = 'Bearer $token';
+
+                // 대기 중인 요청들 처리
+                _resolvePendingRequests(token!);
+
+                // 원래 요청 재시도
+                final response = await _retry(requestOptions);
+                return handler.resolve(response);
+              } else {
+                // 토큰 재발급 실패, 로그인 화면으로 이동
+                // TODO: 로그인 화면으로 이동 로직 구현
+
+                _rejectPendingRequests();
+                return handler.next(error);
+              }
+            } catch (e) {
+              print('토큰 갱신 중 오류: $e');
+              return handler.next(error);
+            } finally {
+              _isRefreshing = false;
+            }
+          }
+
+          return handler.next(error);
+        },
+      ),
+    );
 
     // 로깅 인터셉터 추가
     _dio.interceptors.add(
@@ -39,6 +104,67 @@ class ApiClient {
           return handler.next(e);
         },
       ),
+    );
+  }
+
+  /// 토큰 재발급 요청
+  Future<bool> _refreshToken() async {
+    try {
+      // TODO: 토큰 재발급 API 구현
+      return false;
+    } catch (e) {
+      print('토큰 재발급 실패: $e');
+      return false;
+    }
+  }
+
+  /// 대기 중인 요청에 추가
+  Completer<Response> _addToPendingRequests(RequestOptions requestOptions) {
+    final completer = Completer<Response>();
+    _pendingRequests.add(
+      _RequestRetry(requestOptions: requestOptions, completer: completer),
+    );
+    return completer;
+  }
+
+  /// 대기 중인 요청 처리
+  void _resolvePendingRequests(String token) {
+    for (final request in _pendingRequests) {
+      request.requestOptions.headers['Authorization'] = 'Bearer $token';
+      _retry(request.requestOptions).then(
+        (response) => request.completer.complete(response),
+        onError: (error) => request.completer.completeError(error),
+      );
+    }
+    _pendingRequests.clear();
+  }
+
+  /// 대기 중인 요청 거부
+  void _rejectPendingRequests() {
+    for (final request in _pendingRequests) {
+      request.completer.completeError(
+        DioException(
+          requestOptions: request.requestOptions,
+          error: '토큰 재발급 실패',
+          type: DioExceptionType.unknown,
+        ),
+      );
+    }
+    _pendingRequests.clear();
+  }
+
+  /// 요청 재시도
+  Future<Response> _retry(RequestOptions requestOptions) {
+    final options = Options(
+      method: requestOptions.method,
+      headers: requestOptions.headers,
+    );
+
+    return _dio.request<dynamic>(
+      requestOptions.path,
+      data: requestOptions.data,
+      queryParameters: requestOptions.queryParameters,
+      options: options,
     );
   }
 
@@ -106,6 +232,15 @@ class ApiClient {
   Dio get dio => _dio;
 }
 
+/// 대기 중인 요청 정보를 담는 클래스
+class _RequestRetry {
+  final RequestOptions requestOptions;
+  final Completer<Response> completer;
+
+  _RequestRetry({required this.requestOptions, required this.completer});
+}
+
 final apiClientProvider = Provider<ApiClient>((ref) {
-  return ApiClient();
+  final tokenManager = ref.watch(tokenManagerProvider);
+  return ApiClient(tokenManager);
 });
