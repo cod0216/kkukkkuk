e9964dfa2c732c2c49eef76d0952b8262af722b0
haelim-kweon <khaelim1311@gmail.com>
2025-03-20 16:42:35 +0900

    feat: Service 레이어 logging 추가

--- backend/KKUKKKUK/src/main/java/com/be/KKUKKKUK/global/api/LoggingAspect.java
+++ backend/KKUKKKUK/src/main/java/com/be/KKUKKKUK/global/api/LoggingAspect.java
@@ -25,6 +25,7 @@ import java.util.Map;
  * DATE              AUTHOR             NOTE<br>
  * -----------------------------------------------------------<br>
  * 25.03.15          haelim           최초 생성<br>
+ * 25.03.19          haelim           Service 로깅 추가 <br>
  */
 @Slf4j
 @Aspect
@@ -34,10 +35,28 @@ public class LoggingAspect {
     /**
      * 로그를 적용할 포인트컷을 정의합니다
      * `com.be.KKUKKKUK.domain.*.controller` 패키지의 모든 메서드 실행 시 적용됩니다.
-     * </p>
      */
     @Pointcut("execution(* com.be.KKUKKKUK.domain.*.controller.*.*(..))")
     private void onRequest() {}
+
+    /**
+     * 로그를 적용할 포인트컷을 정의합니다
+     * `com.be.KKUKKKUK.domain.*.service` 패키지의 모든 메서드 실행 시 적용됩니다.
+     */
+    @Pointcut("execution(* com.be.KKUKKKUK.domain.*.service.*.*(..))")
+    private void onService() {}
+
+    /** 컨트롤러 & 서비스 둘 다 로깅합니다. */
+    @Pointcut("onRequest() || onService()")
+    private void onExecution() {}
+
+    /** 메서드 실행 전 로깅 */
+    @Before("onExecution()")
+    public void beforeLog(JoinPoint joinPoint) {
+        log.info("[Before] Method: {} Args: {}",
+                joinPoint.getSignature().toShortString(),
+                params(joinPoint));
+    }
 
     /**
      * 메서드 호출 전에 요청 파라미터 및 HTTP 정보를 로그로 기록합니다.
@@ -57,10 +76,7 @@ public class LoggingAspect {
     }
 
     /**
-     * <p>
      * 응답 객체를 로그로 기록합니다.
-     * </p>
-     *
      * @param joinPoint 호출된 메서드의 정보를 제공하는 JoinPoint 객체
      * @param returnObj 메서드 실행 후 반환된 객체
      */
@@ -70,9 +86,7 @@ public class LoggingAspect {
     }
 
     /**
-     *
      * 메서드의 파라미터 이름과 값을 매핑하여, 로깅을 위한 파라미터 정보를 생성합니다.
-     *
      * @param joinPoint 호출된 메서드의 정보를 제공하는 JoinPoint 객체
      * @return 파라미터 이름과 값의 쌍을 포함하는 맵
      */
