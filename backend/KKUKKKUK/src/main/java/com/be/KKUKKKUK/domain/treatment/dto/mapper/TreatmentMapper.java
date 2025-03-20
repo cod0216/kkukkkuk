511282544717e5eca9a775eef3990aa3892ef89b
haelim-kweon <khaelim1311@gmail.com>
2025-03-21 01:29:49 +0900

    feat: treatment mapper 설정

--- backend/KKUKKKUK/src/main/java/com/be/KKUKKKUK/domain/treatment/dto/mapper/TreatmentMapper.java
+++ backend/KKUKKKUK/src/main/java/com/be/KKUKKKUK/domain/treatment/dto/mapper/TreatmentMapper.java
@@ -1,14 +1,70 @@
 package com.be.KKUKKKUK.domain.treatment.dto.mapper;
 
+import com.be.KKUKKKUK.domain.breed.entity.Breed;
 import com.be.KKUKKKUK.domain.treatment.dto.response.TreatmentResponse;
 import com.be.KKUKKKUK.domain.treatment.entity.Treatment;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Objects;
 
 @Mapper(componentModel = "spring")
 public interface TreatmentMapper {
+    @Mapping(target = "petId", source = "pet.id")
+    @Mapping(target = "petDid", source = "pet.did")
+    @Mapping(target = "name", source = "pet.name")
+    @Mapping(target = "gender", source = "pet.gender")
+    @Mapping(target = "flagNeutering", source = "pet.flagNeutering")
+    @Mapping(target = "birth", source = "pet.birth")
+    @Mapping(target = "dayOfBirth", expression = "java(calculateAge(treatment.getPet().getBirth()))")
+    @Mapping(target = "breedName", expression = "java(formatBreed(treatment.getPet().getBreed()))")
+    @Mapping(target = "age", expression = "java(formatAge(treatment.getPet().getBirth()))")
     List<TreatmentResponse> mapToTreatmentResponseList(List<Treatment> treatmentList);
 
+    @Mapping(source = "pet.id", target = "petId")
+    @Mapping(source = "pet.did", target = "petDid")
+    @Mapping(source = "pet.name", target = "name")
+    @Mapping(source = "pet.gender", target = "gender")
+    @Mapping(source = "pet.flagNeutering", target = "flagNeutering")
+    @Mapping(source = "pet.birth", target = "birth")
+    @Mapping(target = "dayOfBirth", expression = "java(calculateAge(treatment.getPet().getBirth()))")
+    @Mapping(target = "breedName", expression = "java(formatBreed(treatment.getPet().getBreed()))")
+    @Mapping(target = "age", expression = "java(formatAge(treatment.getPet().getBirth()))")
     TreatmentResponse mapToTreatmentResponse(Treatment treatment);
+
+    default Long calculateAge(LocalDate birth) {
+        return (birth != null) ? ChronoUnit.DAYS.between(birth, LocalDate.now()) : null;
+    }
+
+    default String formatAge(LocalDate birth) {
+        if (Objects.isNull(birth)) return null;
+
+        LocalDate now = LocalDate.now();
+        Period period = Period.between(birth, now);
+
+        int years = period.getYears();
+        int months = period.getMonths();
+        int days = period.getDays();
+
+        StringBuilder sb = new StringBuilder();
+
+        if (years > 0) sb.append(years).append("년 ");
+        if (months > 0) sb.append(months).append("개월 ");
+        if (days > 0 && years == 0) sb.append(days).append("일");
+
+        return sb.toString().trim();
+    }
+
+    default String formatBreed(Breed breed) {
+        if (breed == null) {
+            return null;
+        }
+        return (breed.getParentId() != null)
+                ? breed.getParentId().getName() + "(" + breed.getName() + ")"
+                : breed.getName();
+    }
 }
--- backend/KKUKKKUK/src/main/java/com/be/KKUKKKUK/domain/treatment/dto/response/TreatmentResponse.java
+++ backend/KKUKKKUK/src/main/java/com/be/KKUKKKUK/domain/treatment/dto/response/TreatmentResponse.java
@@ -1,21 +1,48 @@
 package com.be.KKUKKKUK.domain.treatment.dto.response;
 
 import com.be.KKUKKKUK.domain.treatment.TreatStatus;
+import com.be.KKUKKKUK.global.enumeration.Gender;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 public class TreatmentResponse {
+    private TreatStatus status;
+
     private Integer id;
 
+    @JsonProperty("pet_id")
     private Integer petId;
 
-    private Integer hospitalId;
+    @JsonProperty("pet_did")
+    private String petDid;
 
+    private String name;
+
+    private LocalDate birth;
+
+    @JsonProperty("day_of_birth")
+    private Long dayOfBirth;
+
+    private String age;
+
+    private Gender gender;
+
+    @JsonProperty("flag_neutering")
+    private Boolean flagNeutering;
+
+    @JsonProperty("breed_name")
+    private String breedName;
+
+    @JsonProperty("expire_date")
     private LocalDate expireDate;
 
-    private TreatStatus status;
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
+
 }
