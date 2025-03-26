eccac35ae889d186949f6213463f606dcf660f63
윤상묵 <smyuau@gmail.com>
2025-03-26 11:58:39 +0900

    feat: hospital signup request interface

---
+++ frontend/src/interfaces/auth.signup.ts
@@ -0,0 +1,24 @@
+/**
+ * @module SignUpRequest
+ * @file auth.signup.ts
+ * @author sangmuk
+ * @date 2025-03-26
+ * @description 회원가입 요청 객체 구조입니다.
+ *
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025-03-26        sangmuk         최초 생성
+ */
+
+/**
+ * signup request interface
+ * @interface
+ */
+export interface SignUpRequest {
+  account: string
+  password: string
+  email: string
+  id: number
+  doctorName: string
+}
