f4289792561815d9e532c22f6f6685032e4f63b8
haelim-kweon <khaelim1311@gmail.com>
2025-03-27 15:27:25 +0900

    docs: useAppNavigation js doc 작성

--- frontend/src/hooks/useAppNavigation.ts
+++ frontend/src/hooks/useAppNavigation.ts
@@ -1,5 +1,19 @@
 import { useNavigate } from "react-router-dom";
 
+/**
+ * @module useAppNavigation
+ * @file useAppNavigation.ts
+ * @author haelim
+ * @date 2025-03-27
+ * @description 네비게이션 사용을 위한 커스텀 훅입니다.
+ *
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025-03-27        haelim         최초 생성
+ */
+
+
 const useAppNavigation = () => {
   const navigate = useNavigate();
 
