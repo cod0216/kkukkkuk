2b08a0693d0ab83afbfdecd37cceb1ad808ce102
hyeonjaez <jhjh0022@naver.com>
2025-03-28 10:25:23 +0900

    feat: id 와 parent id 가 null 인 Breed entity 찾기

--- backend/KKUKKKUK/src/main/java/com/be/KKUKKKUK/domain/breed/repository/BreedRepository.java
+++ backend/KKUKKKUK/src/main/java/com/be/KKUKKKUK/domain/breed/repository/BreedRepository.java
@@ -4,6 +4,7 @@ import com.be.KKUKKKUK.domain.breed.entity.Breed;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * packageName    : com.be.KKUKKKUK.domain.breed.repository<br>
@@ -20,4 +21,6 @@ public interface BreedRepository extends JpaRepository<Breed, Integer> {
     List<Breed> findBreedByParentIdIsNull();
 
     List<Breed> findBreedByParentId(Breed parent);
+
+    Optional<Breed> findByIdAndParentIdIsNull(Integer id);
 }
