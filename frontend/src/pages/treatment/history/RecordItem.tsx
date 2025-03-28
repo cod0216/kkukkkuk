fe29498b94e0fa9a6953265eb4983e4d407e1add
cod0216 <cod0216@nate.com>
2025-03-28 10:50:00 +0900

    chore: path modify

--- frontend/src/pages/treatment/history/RecordItem.tsx
+++ frontend/src/pages/treatment/history/RecordItem.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BlockChainRecord } from '@/interfaces';
+import React from "react";
+import { BlockChainRecord } from "@/interfaces/index";
 
 /**
  * @module RecordItem
@@ -7,7 +7,7 @@ import { BlockChainRecord } from '@/interfaces';
  * @author haelim
  * @date 2025-03-26
  * @description 반려동물 의료 기록의 간략한 정보를 표시하는 UI 컴포넌트
- * 
+ *
  * ===========================================================
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
@@ -16,20 +16,24 @@ import { BlockChainRecord } from '@/interfaces';
 
 /**
  * RecordItem 컴포넌트의 Props 타입 정의
- * @param record 해당 컴포넌트에 정보를 뿌려주기 위한 블록체인 데이터 
+ * @param record 해당 컴포넌트에 정보를 뿌려주기 위한 블록체인 데이터
  * @param idx 해당 컴포넌트의 index
  * @param setSelectedRecordIndex 현재 선택된 블록체인 기록의 index
  */
 interface RecordItemProps {
   record: BlockChainRecord;
   idx: number;
-  setSelectedRecordIndex: (idx:number) => void;
+  setSelectedRecordIndex: (idx: number) => void;
 }
 
 /**
  * 반려동물 의료 기록의 간략한 정보를 표시하는 UI 컴포넌트
  */
-const RecordItem: React.FC<RecordItemProps> = ({ record, idx, setSelectedRecordIndex }) => {
+const RecordItem: React.FC<RecordItemProps> = ({
+  record,
+  idx,
+  setSelectedRecordIndex,
+}) => {
   const HOSPITAL_ADDRESS = "hospital:0xexampleaddress";
   const isMyRecord: boolean = HOSPITAL_ADDRESS === record.hospitalAddress;
 
@@ -41,24 +45,48 @@ const RecordItem: React.FC<RecordItemProps> = ({ record, idx, setSelectedRecordI
     <div
       key={idx}
       onClick={() => setSelectedRecordIndex(idx)}
-      className="hover:bg-gray-50 border-b px-5 py-2 cursor-pointer group flex justify-between items-center">
-
+      className="hover:bg-gray-50 border-b px-5 py-2 cursor-pointer group flex justify-between items-center"
+    >
       <div className="flex flex-col">
-        <div className="font-semibold text-sm text-gray-800">{record.diagnosis}</div>
+        <div className="font-semibold text-sm text-gray-800">
+          {record.diagnosis}
+        </div>
         <div className="text-xs text-gray-500 flex gap-2 items-center">
-          {examinations > 0 && <span className="flex items-center gap-1"> <span className="rounded-max w-1.5 h-1.5 bg-red-200"></span> {examinations}</span>}
-          {medications > 0 && <span className="flex items-center gap-1"> <span className="rounded-max w-1.5 h-1.5 bg-secondary-200"></span>{medications}</span>}
-          {vaccinations > 0 && <span className="flex items-center gap-1"> <span className="rounded-max w-1.5 h-1.5 bg-primary-300"></span>{vaccinations}</span>}
+          {examinations > 0 && (
+            <span className="flex items-center gap-1">
+              {" "}
+              <span className="rounded-max w-1.5 h-1.5 bg-red-200"></span>{" "}
+              {examinations}
+            </span>
+          )}
+          {medications > 0 && (
+            <span className="flex items-center gap-1">
+              {" "}
+              <span className="rounded-max w-1.5 h-1.5 bg-secondary-200"></span>
+              {medications}
+            </span>
+          )}
+          {vaccinations > 0 && (
+            <span className="flex items-center gap-1">
+              {" "}
+              <span className="rounded-max w-1.5 h-1.5 bg-primary-300"></span>
+              {vaccinations}
+            </span>
+          )}
         </div>
       </div>
       <div className="flex flex-col items-end text-right">
-        <div className={
-          `${isMyRecord ? `bg-primary-200` : `bg-secondary-100`} text-[8pt] text-gray-600 font-medium rounded-xl  px-2`
-        }>{record.hospitalName}</div>
-        <div className="font-md text-[8pt] text-gray-500 px-1">{record.createdAt}</div>
-
+        <div
+          className={`${
+            isMyRecord ? `bg-primary-200` : `bg-secondary-100`
+          } text-[8pt] text-gray-600 font-medium rounded-xl  px-2`}
+        >
+          {record.hospitalName}
+        </div>
+        <div className="font-md text-[8pt] text-gray-500 px-1">
+          {record.createdAt}
+        </div>
       </div>
-
     </div>
   );
 };
--- frontend/src/pages/treatment/layout/PetListItem.tsx
+++ frontend/src/pages/treatment/layout/PetListItem.tsx
@@ -1,27 +1,26 @@
-import React from 'react';
-import { FaPaw } from 'react-icons/fa';
-import { Treatment , Gender, TreatmentState } from '@/interfaces';
+import React from "react";
+import { FaPaw } from "react-icons/fa";
+import { Treatment, Gender, TreatmentState } from "@/interfaces/index";
 /**
  * @module PetListItem
  * @file PetListItem.tsx
  * @author haelim
  * @date 2025-03-26
- * @description 진단 페이지 좌측 동물 목록 조회의 각 아이템 UI 컴포넌트입니다. 
- * 
+ * @description 진단 페이지 좌측 동물 목록 조회의 각 아이템 UI 컴포넌트입니다.
+ *
  * ===========================================================
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 2025-03-26        haelim           최초 생성
  */
 
-
 /**
- * PetListItem 컴포넌트의 Props 타입 정의 
- * @param treatment WAS 서버에서 조회한 진료 기록 메타데이터 
- * @param isSelected 현재 메인으로 보여주고 있는 반려동물인지 여부 
- * @param onSelect 메인으로 조회할 반려동물 선택하는 메서드 
- * @param getStateColor 해당 반려동물의 STATE 에 따라 컴포넌트의 색상 결정 
- * @param getStateBadgeColor 해당 반려동물의 STATE 에 따라 컴포넌트의 배지 색상 결정 
+ * PetListItem 컴포넌트의 Props 타입 정의
+ * @param treatment WAS 서버에서 조회한 진료 기록 메타데이터
+ * @param isSelected 현재 메인으로 보여주고 있는 반려동물인지 여부
+ * @param onSelect 메인으로 조회할 반려동물 선택하는 메서드
+ * @param getStateColor 해당 반려동물의 STATE 에 따라 컴포넌트의 색상 결정
+ * @param getStateBadgeColor 해당 반려동물의 STATE 에 따라 컴포넌트의 배지 색상 결정
  */
 interface PetListItemProps {
   treatment: Treatment;
@@ -35,29 +34,42 @@ interface PetListItemProps {
  * 진단 페이지 좌측 동물 목록 조회의 각 아이템 UI 컴포넌트
  */
 const PetListItem: React.FC<PetListItemProps> = ({
-  treatment, isSelected, onSelect, getStateColor, getStateBadgeColor }) => {
+  treatment,
+  isSelected,
+  onSelect,
+  getStateColor,
+  getStateBadgeColor,
+}) => {
   return (
     <div
       className={`flex flex-col gap-1 p-3 rounded-lg transition-all duration-200 cursor-pointer
-        ${isSelected ? 'outline-primary-500' : ''} 
+        ${isSelected ? "outline-primary-500" : ""} 
         ${getStateColor(treatment.state)}`}
       onClick={onSelect}
     >
       <div className="flex gap-2">
         <div className="flex items-center flex-1 justify-between">
-           <div className="flex items-baseline gap-2">
-              <FaPaw className="h-3 w-3 text-gray-500" />
-              <div className="font-bold text-gray-900 text-nowrap">{treatment.name}</div>
-              <div className="text-[8pt] font-medium text-gray-600">{treatment.breedName}</div>
+          <div className="flex items-baseline gap-2">
+            <FaPaw className="h-3 w-3 text-gray-500" />
+            <div className="font-bold text-gray-900 text-nowrap">
+              {treatment.name}
+            </div>
+            <div className="text-[8pt] font-medium text-gray-600">
+              {treatment.breedName}
+            </div>
           </div>
-          <div className={`h-4 flex items-center text-[8pt] px-2 rounded-full text-nowrap ${getStateBadgeColor(treatment.state)}`}>
+          <div
+            className={`h-4 flex items-center text-[8pt] px-2 rounded-full text-nowrap ${getStateBadgeColor(
+              treatment.state
+            )}`}
+          >
             {treatment.state}
           </div>
         </div>
       </div>
       <div className="text-xs text-gray-600">
-        {treatment.age}, {treatment.gender === Gender.MALE ? '수컷' : '암컷'}
-        {treatment.flagNeutering ? ', 중성화' : ''}
+        {treatment.age}, {treatment.gender === Gender.MALE ? "수컷" : "암컷"}
+        {treatment.flagNeutering ? ", 중성화" : ""}
       </div>
     </div>
   );
