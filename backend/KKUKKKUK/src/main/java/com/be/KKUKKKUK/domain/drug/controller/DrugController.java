d0cc4aa121ca6aea0ebfd01c6f3509bdd41bbca1
cod0216 <cod0216@nate.com>
2025-04-02 10:01:12 +0900

    fix: add @AuthenticationPrincipal

--- backend/KKUKKKUK/src/main/java/com/be/KKUKKKUK/domain/drug/controller/DrugController.java
+++ backend/KKUKKKUK/src/main/java/com/be/KKUKKKUK/domain/drug/controller/DrugController.java
@@ -4,6 +4,7 @@ package com.be.KKUKKKUK.domain.drug.controller;
 import com.be.KKUKKKUK.domain.drug.dto.response.DrugResponse;
 import com.be.KKUKKKUK.domain.drug.entity.Drug;
 import com.be.KKUKKKUK.domain.drug.service.DrugService;
+import com.be.KKUKKKUK.domain.hospital.dto.HospitalDetails;
 import com.be.KKUKKKUK.global.util.ResponseUtility;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -11,6 +12,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,7 +30,7 @@ import java.util.List;
  * DATE              AUTHOR             NOTE<br>
  * -----------------------------------------------------------<br>
  * 25.04.01          eunchang           최초 생성<br>
- * 25.04.02          eunchang           약품 자동완성<br>
+ * 25.04.02          eunchang           약품 자동완성 및 @AuthenticationPrincipal 추가 <br>
  */
 @Tag(name = "약품 조회 API", description = "병원에서 약품을 조회할 수 있는 API입니다.")
 @RestController
@@ -52,7 +54,9 @@ public class DrugController {
             @ApiResponse(responseCode = "200", description = "약품 검색 성공")
     })
     @GetMapping("/autocomplete")
-    public ResponseEntity<?> autocompleteDrugs(@RequestParam("query") String query) {
+    public ResponseEntity<?> autocompleteDrugs(
+            @AuthenticationPrincipal HospitalDetails hospitalDetails,
+            @RequestParam("query") String query) {
         List<DrugResponse> responses = drugService.searchDrugResponses(query);
         return ResponseUtility.success("검색어에 따른 약품 목록입니다.", responses);
     }
@@ -62,7 +66,9 @@ public class DrugController {
             @ApiResponse(responseCode = "200", description = "약품 자동완성 조회 성공")
     })
     @GetMapping("/autocorrect")
-    public ResponseEntity<?> autocorrectDrugs(@RequestParam("query") String query) {
+    public ResponseEntity<?> autocorrectDrugs(
+            @AuthenticationPrincipal HospitalDetails hospitalDetails,
+            @RequestParam("query") String query) {
         List<String> responses = drugService.autocorrect(query);
         return ResponseUtility.success("검색어에 따른 약품 자동완성 목록입니다.", responses);
     }
@@ -73,7 +79,9 @@ public class DrugController {
             @ApiResponse(responseCode = "404", description = "해당 약품을 찾을 수 없습니다.")
     })
     @GetMapping
-    public ResponseEntity<?> getDrug(@RequestParam("query") String query) {
+    public ResponseEntity<?> getDrug(
+            @AuthenticationPrincipal HospitalDetails hospitalDetails,
+            @RequestParam("query") String query) {
         DrugResponse responses = drugService.getDrug(query);
         return ResponseUtility.success("검색어에 따른 약품 입니다.", responses);
     }
