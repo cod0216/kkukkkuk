f7e1cd85b1d72c230a88f1ca686bac4c7d46a251
송명석 <sms4621875@naver.com>
2025-04-03 15:55:51 +0900

    fix: ocr service import 오류 해결

--- clientend/lib/controllers/medical_record_register_controller.dart
+++ clientend/lib/controllers/medical_record_register_controller.dart
@@ -1,31 +1,32 @@
+import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kkuk_kkuk/domain/usecases/pet/pet_usecase_providers.dart';
 import 'package:kkuk_kkuk/providers/pet/pet_medical_record_register_provider.dart';
+import 'package:kkuk_kkuk/services/ocr_service.dart';
 
 class MedicalRecordRegisterController {
   final Ref ref;
+  final OcrService _ocrService = OcrService();
 
   MedicalRecordRegisterController(this.ref);
 
-  Future<void> registerMedicalRecord(String petDid) async {
+  Future<Map<String, dynamic>> processImage(File image) async {
+    final ocrData = await _ocrService.processImage(image);
+    return await ref
+        .read(processMedicalRecordImageUseCaseProvider)
+        .execute(ocrData);
+  }
+
+  Future<void> registerMedicalRecord(
+    String petDid,
+    Map<String, dynamic> processedData,
+  ) async {
     await ref
         .read(medicalRecordRegisterProvider.notifier)
-        .registerMedicalRecord(petDid);
-  }
-
-  bool isLoading() {
-    return ref.read(medicalRecordRegisterProvider).isLoading;
-  }
-
-  String? getError() {
-    return ref.read(medicalRecordRegisterProvider).error;
-  }
-
-  String? getTransactionHash() {
-    return ref.read(medicalRecordRegisterProvider).transactionHash;
+        .registerMedicalRecord(petDid, processedData);
   }
 }
 
-final medicalRecordRegisterControllerProvider =
-    Provider<MedicalRecordRegisterController>((ref) {
-      return MedicalRecordRegisterController(ref);
-    });
+final medicalRecordRegisterControllerProvider = Provider((ref) {
+  return MedicalRecordRegisterController(ref);
+});
