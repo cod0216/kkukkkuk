6a62e4e472aef6ef9d9adeb67fca4a1063c54216
송명석 <sms4621875@naver.com>
2025-04-03 21:32:04 +0900

    refactor: wallet usecase 삭제
    unused

--- clientend/lib/domain/usecases/block_chain/connection/blockchain_connection_usecase_providers.dart
+++ clientend/lib/domain/usecases/block_chain/connection/blockchain_connection_usecase_providers.dart
@@ -2,7 +2,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kkuk_kkuk/data/datasource/contracts/blockchain_client.dart';
 import 'package:kkuk_kkuk/domain/usecases/block_chain/connection/check_blockchain_connection_usecase.dart';
 import 'package:kkuk_kkuk/domain/usecases/block_chain/connection/get_web3_client_usecase.dart';
-import 'package:kkuk_kkuk/domain/usecases/wallet/get_wallet_balance_usecase.dart';
 
 final checkBlockchainConnectionUseCaseProvider =
     Provider<CheckBlockchainConnectionUseCase>((ref) {
@@ -13,11 +12,4 @@ final checkBlockchainConnectionUseCaseProvider =
 final getWeb3ClientUseCaseProvider = Provider<GetWeb3ClientUseCase>((ref) {
   final client = ref.watch(blockchainClientProvider);
   return GetWeb3ClientUseCase(client);
-});
-
-final getWalletBalanceUseCaseProvider = Provider<GetWalletBalanceUseCase>((
-  ref,
-) {
-  final client = ref.watch(blockchainClientProvider);
-  return GetWalletBalanceUseCase(client);
 });
